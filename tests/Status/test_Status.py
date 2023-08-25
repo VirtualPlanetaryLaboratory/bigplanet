87ef9894f2b0aae0d634b9174dbd73a05759a0f1
Rory Barnes <rory@proxima.astro.washington.edu>
2023-08-25 14:26:27 -0700

    Trying to ignore corrupt MD5 checksums in tests/Status

--- tests/Status/test_Status.py
+++ tests/Status/test_Status.py
@@ -28,7 +28,7 @@ def test_bpstatus():
 
         # Run bigplanet
         if not (path / ".BP_Status_BPL").exists():
-            subprocess.check_output(["bigplanet", "bpl.in", "-a"], cwd=path)
+            subprocess.check_output(["bigplanet", "-ignorecorrupt", "bpl.in", "-a"], cwd=path)
             subprocess.check_output(["bpstatus", "vspace.in"], cwd=path)
 
         file = path / "BP_Status.bpa"
